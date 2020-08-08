ce52d6dc961ab770d087120e2c8bfb917b950321
roomanidzee <steampart@gmail.com>
2020-08-08 20:39:15 +0300

    hello sonar my old friend

--- project/Sonar.scala
+++ project/Sonar.scala
@@ -5,8 +5,15 @@ object Sonar {
     "sonar.organization" -> "romanidze-projects",
     "sonar.projectName" -> "kaesquella",
     "sonar.projectKey" -> "romanidze_kaesquella",
-    "sonar.modules" -> "modules/core",
+    "sonar.sourceEncoding" -> "UTF-8",
+    "sonar.scala.version" -> "2.13.2",
+
+    "sonar.modules" -> "kaesquella-core",
+
     "kaesquella-core.sonar.projectName" -> "kaesquella-core",
+    "kaesquella-core.sonar.sources" -> "modules/core/src/main/scala",
+    "kaesquella-core.sonar.tests" -> "modules/core/src/test/scala",
+
     "sonar.sourceEncoding" -> "UTF-8",
     "sonar.scala.scoverage.reportPath" -> "target/scala-2.13/scoverage-report/scoverage.xml",
     "sonar.scala.coverage.reportPaths" -> "target/scala-2.13/scoverage-report/scoverage.xml",
