e23ff644b791a5c69d006b2a676b67065bbc4f2d
roomanidzee <steampart@gmail.com>
2020-08-09 16:23:55 +0300

    another attempt

--- project/Sonar.scala
+++ project/Sonar.scala
@@ -7,11 +7,15 @@ object Sonar {
     "sonar.projectKey" -> "romanidze_kaesquella",
     "sonar.sourceEncoding" -> "UTF-8",
     "sonar.scala.version" -> "2.13.2",
-
-    "sonar.modules" -> "modules/core",
-
     "sonar.sourceEncoding" -> "UTF-8",
-    "sonar.scala.scoverage.reportPath" -> "target/scala-2.13/scoverage-report/scoverage.xml",
-    "sonar.scala.coverage.reportPaths" -> "target/scala-2.13/scoverage-report/scoverage.xml",
+
+    "sonar.projectBaseDir" -> "modules",
+
+    "sonar.modules" -> "core",
+
+    "core.sonar.sources" -> "src/main/scala",
+    "core.sonar.tests"-> "src/test/scala",
+    "core.sonar.scala.scoverage.reportPath" -> "target/scala-2.13/scoverage-report/scoverage.xml",
+    "core.sonar.scala.coverage.reportPaths" -> "target/scala-2.13/scoverage-report/scoverage.xml"
   )
 }
